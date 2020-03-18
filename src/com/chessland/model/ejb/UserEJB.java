73aeca5231155873ba2efd9b03382ec3e98b67bd
Gustav <gustavokurpel@gmail.com>
2020-03-18 05:03:34 +0100

    Añadido las anotaciones para que la clase funcione como un ejb.

--- src/com/chessland/model/ejb/UserEJB.java
+++ src/com/chessland/model/ejb/UserEJB.java
@@ -2,9 +2,14 @@ package com.chessland.model.ejb;
 
 import java.util.List;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
 import com.chessland.model.dao.UserDAO;
 import com.chessland.model.pojo.User;
 
+@Stateless
+@LocalBean
 public class UserEJB {
 
 	public List<User> getAll() {
