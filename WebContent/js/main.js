bc92fc1f40ab8d7341a5fb1306beaa425b4ec413
fd <fds>
2020-03-23 16:09:41 +0100

    Se cambió el método de javaScript para que cambie de página a la dirección indicada por el servidor.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -5,8 +5,15 @@ $(document).ready(function() {
 function onSignIn(googleUser) {
 	let idToken = googleUser.getAuthResponse().id_token;
 	console.log(idToken);
+
 	let request = new XMLHttpRequest();
 	request.open('POST', 'Login');
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	request.setRequestHeader('Content-Type',
+			'application/x-www-form-urlencoded');
+	request.onreadystatechange = function() {
+		if (this.status === 200 && this.readyState === 4) {
+			window.location.href = this.responseText;
+		}
+	}
 	request.send('token=' + idToken);
 }
