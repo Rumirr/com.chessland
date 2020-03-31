1b87225f7842ba4a7bc83f47bc274d8b4927b97e
fd <fds>
2020-03-31 17:55:10 +0200

    Servlet encargado de invalidar la sesión de un usuario loggeado.

---
+++ src/com/chessland/controller/Logout.java
@@ -0,0 +1,37 @@
+package com.chessland.controller;
+
+import java.io.IOException;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.chessland.model.ejb.SessionEJB;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	SessionEJB sessionEJB;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+
+		if (sessionEJB.hasUser(session)) {
+			session.invalidate();
+
+		}
+		response.sendRedirect("Login");
+
+	}
+
+}
